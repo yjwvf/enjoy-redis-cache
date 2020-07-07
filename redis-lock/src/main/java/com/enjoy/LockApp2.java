5c0561bec5a24cb0d12fec1709738c8f5d3a0ec3
yjwvf <1009626711@qq.com>
2020-07-08 04:54:41 +0800

    v0.1 add LockApp2.java

--- redis-lock/src/main/java/com/enjoy/LockApp2.java
+++ redis-lock/src/main/java/com/enjoy/LockApp2.java
@@ -8,6 +8,7 @@ public class LockApp2 {
     public static void main(String[] args) {
         System.out.println("test init");
         System.out.println("v0.1 add");
+        System.out.println("v0.2 add 2");
         SpringApplication.run(LockApp2.class, args);
     }
 }
